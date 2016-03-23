8298ce4809eb1322db9cdcde1d08409542b9eda7
Rana <eng.ranafouad.2013@gmail.com>
2016-03-23 14:42:17 +0200

    database

--- AqarMap/settings.py
+++ AqarMap/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aqar',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,9 +77,12 @@ WSGI_APPLICATION = 'AqarMap.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AqarMap',
+        'HOST':'localhost',
+        'USER':'x',
+        'PASSWORD':'iti',
     }
 }
 
--- aqar/models.py
+++ aqar/models.py
@@ -2,4 +2,53 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class user_profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    phone=models.IntegerField()
+    currancy=models.CharField(max_length=100)
+    type=models.CharField(max_length=100)
+    country=models.CharField(max_length=100)
+
+class property(models.Model):
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE)
+    category=models.CharField(max_length=100)
+    type=models.CharField(max_length=100)
+    sellerType=models.CharField(max_length=100)
+    size=models.CharField(max_length=100)
+    price=models.IntegerField()
+    title=models.CharField(max_length=300)
+    description=models.CharField(max_length=500)
+    city=models.CharField(max_length=100)
+    area=models.CharField(max_length=100)
+    street=models.CharField(max_length=100)
+    services=models.CharField(max_length=100)
+    facilities=models.CharField(max_length=100)
+    picture=models.CharField(max_length=100)
+    payment=models.CharField(max_length=100)
+
+class project(models.Model):
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE)
+    detailes=models.CharField(max_length=500)
+    services=models.CharField(max_length=100)
+    facilities=models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+
+class project_property(models.Model):
+    project_id=models.ForeignKey(project, on_delete=models.CASCADE)
+    category=models.CharField(max_length=100)
+    type=models.CharField(max_length=100)
+    sellerType=models.CharField(max_length=100)
+    size=models.CharField(max_length=100)
+    price=models.IntegerField()
+    title=models.CharField(max_length=300)
+    description=models.CharField(max_length=500)
+    city=models.CharField(max_length=100)
+    area=models.CharField(max_length=100)
+    street=models.CharField(max_length=100)
+    services=models.CharField(max_length=100)
+    facilities=models.CharField(max_length=100)
+    picture=models.CharField(max_length=100)
+    payment=models.CharField(max_length=100)
